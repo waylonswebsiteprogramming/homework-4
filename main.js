4ec36a4eb3d50b11e0ac221f36336eb5bb20e183
waylon knudsen <waylonknudsen1@gmail.com>
2017-05-29 23:07:08 -0600

    v1.0.0

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('body').style.backgroundColor = "red";
+document.getElementById('heading').style.backgroundColor = "blue";
+document.getElementById('body').style.fontFamily = "sans-serif";
+document.getElementById('nickname').innerHTML = "Tazz";
+document.getElementById('favorites').innerHTML = "Programming";
+document.getElementById('hometown').innerHTML = "Salt Lake City";
